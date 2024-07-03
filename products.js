9e391b836abecf69d497e843a306bbfdc7d64876
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-07-04 00:44:38 +0300

    Delete products.js

--- products.js
+++
@@ -1,7 +0,0 @@
-// Add event listeners to add products to the cart
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Add code here to handle adding the product to the cart
-  });
-});
